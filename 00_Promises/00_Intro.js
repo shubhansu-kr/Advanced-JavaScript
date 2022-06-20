4a7bdec38152198522ab377174406056cee489c6
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-20 14:44:46 +0530

    Stop Watch

---
+++ 00_Promises/00_Intro.js
@@ -0,0 +1,62 @@
+var minute = document.querySelector("#minute");
+var seconds = document.querySelector("#seconds");
+var milisec = document.querySelector("#milisec");
+
+var start = document.querySelector("#start-button");
+var pause = document.querySelector("#pause-button");
+var reset = document.querySelector("#reset-button");
+
+let isStarted = false, isPaused = true;
+let count = 0;
+
+let minuteCount = 0, secondsCount = 0, milisecCount = 0;
+
+start.addEventListener("click", () => {
+    isStarted = true;
+    isPaused = false;
+});
+
+pause.addEventListener("click", () => {
+    if (isPaused == false) {
+        pause.innerText = "Play";
+        isPaused = true;
+        isStarted = false;
+    }
+    else {
+        isPaused = false;
+        isStarted = true;
+        pause.innerText = "Pause";
+    }
+});
+
+reset.addEventListener("click", () => {
+    milisecCount = secondsCount = minuteCount = 0;
+    isStarted = false;
+    isPaused = true;
+    pause.innerText = "Pause";
+});
+
+setInterval(() => {
+    if (isStarted) {
+        milisecCount++;
+        if (milisecCount === 220) {
+            secondsCount++;
+            milisecCount = 0;
+        }
+        if (secondsCount == 59) {
+            minuteCount++;
+            secondsCount = 0;
+        }
+    }
+    milisec.innerText = milisecCount;
+    seconds.innerText = secondsCount;
+    minute.innerText = minuteCount;
+}, 1);
+
+
+var timer = document.querySelector("#time");
+
+setInterval(() => {
+    timer.innerText = new Date().toLocaleString();
+}, 1000);
+
